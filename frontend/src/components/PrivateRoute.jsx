db005730395dddec421c926638024a38d04c6556
prageeth dananjaya <e19431@eng.pdn.ac.lk>
2025-10-14 16:00:32 +0530

    feat(auth): add PrivateRoute component for protected routes

---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,7 @@
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export default function PrivateRoute({ children }) {
+  const { token } = useSelector((state) => state.auth);
+  return tokrn ? children : <Navigate to="/" replace />;
+}
