e1f6322b386cb5f1c9c0066deba95de0659311ff
prageeth dananjaya <e19431@eng.pdn.ac.lk>
2025-10-14 09:11:46 +0530

    feat(auth-ui): add LoginPage with Redux login functionality

---
+++ frontend/src/features/auth/LoginPage.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "./authSlice";
+import { useNavigate } from "react-router-dom";
+
+export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useSate("");
+  const { loading, error } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const result = await dispatch(loginUser({ username, password }));
+    if (result.meta.requestStatus === "fulfilled") {
+      navigate("/dashboard");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit" disabled={loading}>
+          {loading ? "Logging in ..." : "Login"}
+        </button>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </form>
+    </div>
+  );
+}
